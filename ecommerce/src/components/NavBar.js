d12cb1ea639478d252cceb10a1caaa06eb22d982
Arsh-Sharan <arshsharan06@gmail.com>
2024-10-30 11:46:59 +0530

    Added cart button

--- ecommerce/src/components/NavBar.js
+++ ecommerce/src/components/NavBar.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/NavBar.css';
 
-function NavBar() {
+function NavBar({ toggleCart, cartItemCount }) {
   return (
     <nav className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/products">Products</Link>
+      <Link to="/" className="navbar-logo">FakeStore</Link>
+      <div className="navbar-links">
+        <Link to="/products">Products</Link>
+        <button className="cart-button" onClick={toggleCart}>
+          Cart ({cartItemCount})
+        </button>
+      </div>
     </nav>
   );
 }
